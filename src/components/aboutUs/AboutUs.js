8b8a8acee8b39d77332a0b796f4b97167daa5b41
mike21021 <angel210521@gmail.com>
2024-02-03 19:27:00 -0600

    information about us corrected

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -3,22 +3,24 @@ import styles from "./aboutus.module.css";
 export default function AboutUs() {
     return(
         <div className={styles.behind_section_about_us}>
-            <div>
+            <div className={styles.paragraph_container}>
                 <h1>About Us</h1>
                 <span>
-                    Problems trying to resolve the conflict between
+                    Problems trying to resolve the conflict between <br/>
                     the two major realms of Classical physics: Newtonian mechanics
                 </span>
             </div>
-            <div>
+            <div className={styles.row_media_txt}>
                 <div>video</div>
-                <div>
-                    <h2>Most trusted in our field</h2>
-                    <p>
-                        Most calendars are designed for teams. Slate is designed for
-                        freelancers who want a simple way to plan their schedule.
-                    </p>
-                    <div>
+                <div className={styles.column_txt_us}>
+                    <div className={styles.paragraph_container}>
+                        <h3>Most trusted in our field</h3>
+                        <p>
+                            Most calendars are designed for teams. Slate is designed for
+                            freelancers who want a  simple way to plan their schedule.
+                        </p>
+                    </div>
+                    <div className={styles.txt_quick_fox}>
                         <span>
                             <b>
                                 the quick fox jumps over the lazy dog
